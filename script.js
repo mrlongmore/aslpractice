23444254d1c25f0f39190d09d25f17480b2e5a02
mrlongmore <hlongmore@fuhsd.net>
2026-04-08 00:29:13 +0000

    First commit, rough draft.

---
+++ script.js
@@ -0,0 +1,6 @@
+name_modal = getElementById('name-modal');
+topic_modal = getElementById('topic-modal');
+quiz_modal = getElementById('quiz-modal');
+certificate_modal = getElementById('certificate-modal')
+
+name_modal.classList.add("active");
