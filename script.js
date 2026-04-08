2315328d7fbd4d83e9d91d2df7d73b4022bad387
mrlongmore <hlongmore@fuhsd.net>
2026-04-08 00:46:57 +0000

    JS Adjustment #1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-name_modal = getElementById('name-modal');
-topic_modal = getElementById('topic-modal');
-quiz_modal = getElementById('quiz-modal');
-certificate_modal = getElementById('certificate-modal')
+name_modal = document.getElementById('name-modal');
+topic_modal = document.getElementById('topic-modal');
+quiz_modal = document.getElementById('quiz-modal');
+certificate_modal = document.getElementById('certificate-modal')
 
 name_modal.classList.add("active");
