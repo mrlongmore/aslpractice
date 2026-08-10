9a51543934048603c1afc9a94e0f08f5d88eee35
mrlongmore <ngulongmore@aol.com>
2026-08-10 14:09:39 -0700

    Refactor percentage calculation into displayCertificate
    Removed the calculatePercentage function and integrated its logic directly into the displayCertificate function.

--- script.js
+++ script.js
@@ -135,12 +135,9 @@ function startQuiz(questions) {
     displayQuestion();
 }
 
-function calculatePercentage() {
-    percentage_correct = questions_correct / questions_total;
-}
 // Certificate modal population
 function displayCertificate() {
-    calculatePercentage()
+    percentage_correct = questions_correct/questions_total;
     certificate_header.innerHTML = `<h1>Great work, ${user_name}!</h1>`;
     results_area.innerHTML = `<h1>You answered all ${questions_total} question(s), with ${questions_incorrect} question(s) incorrect and ${questions_correct} question(s) correct. Your percentage correct is ${percentage_correct}%</h1>
     <p>Take a screenshot of your results and upload them to Google Classroom!</p>`;
